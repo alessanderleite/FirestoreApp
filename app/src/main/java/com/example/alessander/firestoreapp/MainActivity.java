30a0f4a7b18c0afa1f01362fb7d7ae745b43bc9e
alessanderleite <alessander.logistica@gmail.com>
2018-11-07 22:51:05 -0200

    delete title and delete descrition

--- app/src/main/java/com/example/alessander/firestoreapp/MainActivity.java
+++ app/src/main/java/com/example/alessander/firestoreapp/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
@@ -63,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
                     String description = documentSnapshot.getString(KEY_DESCRIPTION);
 
                     textViewData.setText("Title: " + title + "\n" + "Description: " + description);
+                } else {
+                    textViewData.setText("");
                 }
             }
         });
@@ -100,6 +103,18 @@ public class MainActivity extends AppCompatActivity {
 
 //        noteRef.set(note, SetOptions.merge());
         noteRef.update(KEY_DESCRIPTION, description);
+    }
+
+    public void deleteDescription(View v) {
+//        Map<String, Object> note = new HashMap<>();
+//        note.put(KEY_DESCRIPTION, FieldValue.delete());
+
+//        noteRef.update(note);
+        noteRef.update(KEY_DESCRIPTION, FieldValue.delete());
+    }
+
+    public void deleteNote(View v) {
+        noteRef.delete();
     }
 
     public void loadNote(View v) {
